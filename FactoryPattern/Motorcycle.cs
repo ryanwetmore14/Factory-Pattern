d218aa97ced6ea3054c359d44894d1d7028de4cf
ryan wetmore <ryanwetmore1014@gmil.com>
2024-05-16 11:57:20 -0400

    completed factory pattern exercise 1

---
+++ FactoryPattern/Motorcycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public  class Motorcycle : IVehicle
+    {
+        public Motorcycle()
+        {
+
+        }
+
+        public void Drive()
+        {
+            ConsoleLogger.VehicleStartUpDialogue();
+            Console.WriteLine("The Motorcycle is revving up");
+            Console.WriteLine("vroooooooooooom");
+        }
+    }
+}
---
+++ FactoryPattern/ThreeWheeler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class ThreeWheeler : IVehicle
+    {
+        public ThreeWheeler()
+        {
+
+        }
+
+        public void Drive()
+        {
+            ConsoleLogger.VehicleStartUpDialogue();
+            Console.WriteLine("Lets get these 3 wheels going!");
+        }
+    }
+}
